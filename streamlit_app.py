f9ab6e89135678230fcd8e4e706c4a7ce708b448
RadiNajeeb <Radinajeeb@gmail.com>
2025-08-18 12:36:01 +0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="Football Data")
+st.title("Welcome to Football Data App ⚽")
